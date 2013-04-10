4db249105ac1a2fbe6936557b15798b27a1dde8b
Sravan Gurijala <shravan.guri@gmail.com>
2013-04-10 09:56:46 -0400

    Initial Commit

---
+++ Models/LoginModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Events.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        [AllowHtml]
+        public string UserName { get; set; }
+        [Required]
+        [AllowHtml]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string cnfmPassword { get; set; }
+    }
+}
---
+++ Models/ProfileModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FluentValidation.Mvc;
+
+namespace Events.Models
+{
+     //[Validator(typeof(RegisterValidator))]
+    public class ProfileModel
+    {
+         [AllowHtml]
+        public string FirstName { get; set; }
+         [AllowHtml]
+        public string LastName { get; set; }
+        [AllowHtml]
+        public LoginModel Login { get; set; }
+    }
+}
