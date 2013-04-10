6e91487643e3d6ada0f919a00c5c7dab5ad2ebf9
Sravan Gurijala <shravan.guri@gmail.com>
2013-04-10 11:28:15 -0400

    edmx removed

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -84,31 +84,31 @@ namespace Events.Controllers
         [HttpPost]
         public ActionResult ProfileCreate(ProfileModel ProfileModel)
         {
-            try
-            {
-                using (Events.Entities EventContext = new Entities())
-                {
-                    T_LOGIN loginEntity = new T_LOGIN();
-                    loginEntity.UserName = ProfileModel.Login.UserName;
-                    loginEntity.Password = ProfileModel.Login.Password;
-                    EventContext.T_LOGIN.Add(loginEntity);
-                    EventContext.SaveChanges();
+            //try
+            //{
+            //    using (Events.Entities EventContext = new Entities())
+            //    {
+            //        T_LOGIN loginEntity = new T_LOGIN();
+            //        loginEntity.UserName = ProfileModel.Login.UserName;
+            //        loginEntity.Password = ProfileModel.Login.Password;
+            //        EventContext.T_LOGIN.Add(loginEntity);
+            //        EventContext.SaveChanges();
                    
-                    T_PROFILE_DETAILS profilesEntity = new T_PROFILE_DETAILS();
-                    profilesEntity.FirstName = ProfileModel.FirstName;
-                    profilesEntity.LastName = ProfileModel.LastName;
-                    profilesEntity.LoginId = loginEntity.LoginId;
-                    EventContext.T_PROFILE_DETAILS.Add(profilesEntity);
-                    EventContext.SaveChanges();
+            //        T_PROFILE_DETAILS profilesEntity = new T_PROFILE_DETAILS();
+            //        profilesEntity.FirstName = ProfileModel.FirstName;
+            //        profilesEntity.LastName = ProfileModel.LastName;
+            //        profilesEntity.LoginId = loginEntity.LoginId;
+            //        EventContext.T_PROFILE_DETAILS.Add(profilesEntity);
+            //        EventContext.SaveChanges();
                                         
-                }
+            //    }
 
-            }
-            catch
-            {
+            //}
+            //catch
+            //{
 
-                return View();
-            }
+            //    return View();
+            //}
 
             return RedirectToAction("ThankYou");
         }
